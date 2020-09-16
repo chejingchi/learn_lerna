1421e6ba7b24ec934b17c15094971b722a9cf636
michael che <1020934146@qq.com>
2020-09-16 17:39:48 +0800

    michaelChe; first step

---
+++ packages/learn-lerna-core/index.js
@@ -0,0 +1,14 @@
+var _ = require("lodash")
+var test = function (params) {
+  console.log("test")
+  console.log("test")
+  console.log("test")
+  console.log("test1111")
+  console.log("test2222")
+  console.log("test2222333")
+}
+
+module.exports = {
+  test,
+  _
+}
---
+++ packages/learn-lerna-module-a/index.js
@@ -0,0 +1,4 @@
+var test = require('learn-lerna-core');
+var _ = test._;
+test.test();
+console.log(_.chunk(['a', 'b', 'c', 'd'], 3));
